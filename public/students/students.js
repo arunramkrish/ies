ae441be9fa1cf1f66f4dd8569e8c87dd8179324c
arunramkrish <arunram.krish@gmail.com>
2017-01-11 09:39:30 +0530

    Added student service to post new student record to server

--- public/students/students.js
+++ public/students/students.js
@@ -6,7 +6,7 @@ angular.module("ies.students", ["ngRoute"])
   });
 }])
 
-.controller('StudentsCtrl', ['$scope', '$rootScope', function(s, root) {
+.controller('StudentsCtrl', ['$scope', '$rootScope', 'StudentService', function(s, root, svc) {
     s.listMode = true;
     s.studentProfiles=[{name:'Sheik', rollNo:'007', programme: root.msg, semester: 8},{name:'Sheikabdullah', rollNo:'007', programme: root.msg},{name:'Sheik', rollNo:'007', programme: root.msg},{name:'Sheik', rollNo:'007', programme: root.msg}];
 
@@ -18,8 +18,23 @@ angular.module("ies.students", ["ngRoute"])
     };
 
     s.save = function() {
-        s.studentProfiles.push(s.studentProfile);
-        s.studentProfile = {};
-        s.listMode = true;
+        svc.create(s.studentProfile, function(response) {
+            s.studentProfiles.push(response.data);
+            s.studentProfile = {};
+            s.listMode = true;
+        });
+    };
+}])
+.service('StudentService', ['$http', function($http) {
+    this.create = function(student, callback) {
+        var request = { 
+            url: 'students',
+            data: JSON.stringify(student),
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        };
+        $http(request).then(callback, function() { 
+            console.log('error in posting students');
+        });
     };
 }]);
